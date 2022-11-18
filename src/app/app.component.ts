7a851b7cf8175295edd0fd4a282e5574e423bb8c
Gennadiy Tsarenkov <gtsarenkov@tut.by>
2022-11-18 15:08:57 +0100

    Made human-readable application title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,5 +28,5 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'integration-jdisc-ui';
+  title = 'Sample of application with Integration for JDisc Web UI';
 }
