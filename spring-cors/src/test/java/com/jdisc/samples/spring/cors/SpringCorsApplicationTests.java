6668447eb07f7664903e87d271dd5aa943a2f22e
Gennadiy Tsarenkov <gtsarenkov@tut.by>
2022-11-21 08:34:28 +0100

    Added Spring CORS configuration for Web UI integration project.

---
+++ spring-cors/src/test/java/com/jdisc/samples/spring/cors/SpringCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdisc.samples.spring.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
